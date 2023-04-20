d7ca4635ea6833dbcc606ba6f69a27e98978154a
Shane <46799448+OfficiallySp@users.noreply.github.com>
2023-04-20 06:53:15 +0100

    Add files via upload

---
+++ execute.py
@@ -0,0 +1,7 @@
+from twitch import createCompilation
+#replace the following on line 4 with name of streamer. time period (past_day, past_week, past_month or all_time, video number, and target duration in minutes)
+def main():
+    createCompilation("name", 'timeperiod', videonum, targetlength)
+
+if __name__ == "__main__":
+    main()
