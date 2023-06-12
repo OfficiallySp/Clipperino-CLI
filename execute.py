cc0226ef8e4bc63148653b32e681c119b092149a
Shane <46799448+OfficiallySp@users.noreply.github.com>
2023-06-12 22:07:30 +0100

    Add files via upload

--- execute.py
+++ execute.py
@@ -1,7 +1,6 @@
-from twitch import createCompilation
-#replace the following on line 4 with name of streamer. time period (past_day, past_week, past_month or all_time, video number, and target duration in minutes)
+from twitch import createCompilationAuto
 def main():
-    createCompilation("name", 'timeperiod', videonum, targetlength)
+    createCompilationAuto("streamers.txt")
 
 if __name__ == "__main__":
     main()
--- twitch.py
+++ twitch.py
@@ -1,13 +1,12 @@
 import os
 import random
-import sys
 import subprocess
 from datetime import datetime
 
 def output(string):
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    print(current_time, "|", string)
+    print(f"{current_time} | {string}")
 
 def getLength(filename):
     result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
@@ -17,17 +16,29 @@ def getLength(filename):
         stderr=subprocess.STDOUT)
     return float(result.stdout)
 
-def createCompilation(streamer, period = 'last_week', videoNum = 0, minDuration = 10):
-    output("Creating compilation for " + streamer + ".")
+def download_clip(clip_url):
+    output(f"Downloading clip: {clip_url}")
+    os.system(f"twitch-dl download -q source {clip_url} > NUL")
+
+def createCompilationAuto(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            streamer, period, videoNum, minDuration = line.strip().split()
+            createCompilation(streamer, period, int(videoNum), int(minDuration))
+
+def createCompilation(streamer, period='last_week', videoNum=0, minDuration=10):
+    output(f"Creating compilation for {streamer}.")
 
     # make output directory if it does not exist
-    os.system('mkdir output > NUL')
+    output_directory = f'output/{streamer}'
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
     output("Retrieving clips.")
 
     # get list of clips from twitch-dl
-    os.system('twitch-dl clips ' + streamer + ' --period ' + period + ' --limit 100 > temp.txt')
-    with open('temp.txt') as f:
+    os.system(f'twitch-dl clips {streamer} --period {period} --limit 100 > temp.txt')
+    with open('temp.txt', encoding='utf-8') as f:
         clipLines = f.readlines()
     os.remove('temp.txt')
 
@@ -39,16 +50,16 @@ def createCompilation(streamer, period = 'last_week', videoNum = 0, minDuration 
             try:
                 tempClip = {}
                 tempClip['id'] = clipLines[i][9:-5]
-                tempClip['title'] = clipLines[i+1][5:-5]
-                tempClip['url'] = clipLines[i+4][4:-5]
-                tempClip['game'] = clipLines[i+2].split('playing')[1][6:-5]
+                tempClip['title'] = clipLines[i + 1][5:-5]
+                tempClip['url'] = clipLines[i + 4][4:-5]
+                tempClip['game'] = clipLines[i + 2].split('playing')[1][6:-5]
                 channelClips.append(tempClip)
                 i = i + 5
             except:
                 i = i + 1
         else:
             i = i + 1
-    
+
     output("Beginning clip download. (THIS WILL TAKE SOME TIME)")
 
     duration = 0
@@ -58,26 +69,27 @@ def createCompilation(streamer, period = 'last_week', videoNum = 0, minDuration 
     titles = []
     invalid = []
     # downloads required clips
-    while((duration < minDuration*60) and i < len(channelClips)):
+    while ((duration < minDuration * 60) and i < len(channelClips)):
         try:
             title = channelClips[i]['title']
         except:
             i += 1
             continue
-        if('\\' in title):
+        if '\\' in title:
             i += 1
             continue
 
         oldFiles = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), f))]
 
-        os.system("twitch-dl download -q source " + channelClips[i]['url'] + " > NUL")
-        
+        download_clip(channelClips[i]['url'])
+
         newFiles = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), f))]
         download = ""
         for file in newFiles:
             if file not in oldFiles:
                 download = file
         if (len(newFiles) == len(oldFiles)) or (download == ""):
+            invalid.append(channelClips[i]['url'])
             i += 1
             continue
 
@@ -88,53 +100,64 @@ def createCompilation(streamer, period = 'last_week', videoNum = 0, minDuration 
                 break
             except:
                 title = title[:-1]
-        if(channelClips[i]['game'] not in games):
+        if (channelClips[i]['game'] not in games):
             games.append(channelClips[i]['game'])
-        downloads.append('output/' + download)
-        os.rename(download, 'output/' + download)
-        
+        downloads.append(f'output/{streamer}/' + download)
+        output_path = f'output/{streamer}/' + download
+        if os.path.exists(download):
+            if os.path.exists(output_path):
+                output(f"File {download} already exists in output directory. Skipping.")
+            else:
+                os.rename(download, output_path)
+                output(f"Downloaded: {title}")
+        else:
+            output(f"File {download} does not exist. Skipping.")
         i += 1
+    if not downloads:  # If the list is empty, no clips were downloaded
+        output(f"No clips could be downloaded for {streamer} in the specified timeframe ({period}). Skipping to next streamer.")
+        return
 
     # join clips
     output("Joining clips.")
     random.shuffle(downloads)
     tsClipLocations = []
-    f= open("temp.txt","w+")
+    f = open("temp.txt", "w+")
     for clip in downloads:
-        #convert to .ts
+        # convert to .ts
         clipname = clip[:-4]
-        os.system("ffmpeg -loglevel error -i " + clipname + ".mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts " + clipname + ".ts")
+        os.system(f"ffmpeg -loglevel error -i {clipname}.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts {clipname}.ts")
         tsClipLocations.append(clipname + ".ts")
-        #concoctenate .ts to mp4
-        f.write("file '" + clipname + ".ts" + "'\n")
-    f.close() 
-    os.system("ffmpeg -loglevel error -f concat -safe 0 -i temp.txt -c copy " + "output" + ".mp4")
+        # concatenate .ts to mp4
+        f.write(f"file '{clipname}.ts'\n")
+    f.close()
+    os.system("ffmpeg -loglevel error -f concat -safe 0 -i temp.txt -c copy output.mp4")
     os.remove("temp.txt")
-    os.system('ffmpeg -loglevel error -err_detect ignore_err -i output.mp4 -c copy output/' + streamer + "_" + str(videoNum) + ".mp4")
+    os.system(f'ffmpeg -loglevel error -err_detect ignore_err -i output.mp4 -c copy output/{streamer}/{streamer}_{videoNum}.mp4')
     os.remove('output.mp4')
 
-    # create textfile
+
+    # create text file
     output("Creating text file.")
-    f= open('output/' + streamer + "_" + str(videoNum) + ".txt","w+")
+    f = open(f'output/{streamer}/{streamer}_{videoNum}.txt', "w+")
     # title
     random.shuffle(titles)
     count = 0
-    while(True): # tries titles til it finds a valid one
+    while(True):  # tries titles until it finds a valid one
         try:
             title = titles[count]
             break
         except:
             count += 1
-        
+
     f.write(title + "\n")
     # description
     desc = streamer + "'s Twitch Channel: https://www.twitch.tv/" + streamer
     f.write(desc + "\n")
     # tags
-    tags = streamer 
+    tags = ""
     for game in games:
-        tags+= game + ', '
-    f.write("Categories" + tags + "\n")
+        tags += game + ', '
+    f.write("Categories: " + tags + "\n")
     f.close()
 
     # clean up
@@ -142,11 +165,13 @@ def createCompilation(streamer, period = 'last_week', videoNum = 0, minDuration 
         try:
             os.remove(file)
         except:
-            output("Unable to delete " + file)
+            output(f"Unable to delete {file}")
     for file in invalid:
         try:
             os.remove(file)
         except:
-            output("Unable to delete " + file)
+            output(f"Unable to delete {file}")
 
-    return 
+    output("Compilation creation completed!\n")
+    return
+    output("COMPLETE")
